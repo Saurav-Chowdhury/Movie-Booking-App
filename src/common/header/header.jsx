efd994cb19ffb5b4c42dea5250479d06fb603120
Saurav-Chowdhury <sauravsonybravia99@gamil.com>
2022-01-23 12:13:58 +0530

    Checkpoint 2

---
+++ src/common/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./header.css";
+const Header = () => {
+    return ( 
+        <div className="header">
+            <img id="logo" src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg" alt="logo" />
+        </div>
+     );
+}
+ 
+export default Header;
